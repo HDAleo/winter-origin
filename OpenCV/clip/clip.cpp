720693e39de057cecd2bc0ff87e23b3719122d9f
HDAleo <3480678832@qq.com>
2024-02-27 17:29:40 +0800

    update opencv/clip

---
+++ OpenCV/clip/clip.cpp
@@ -0,0 +1,55 @@
+#include <opencv2/opencv.hpp>  
+#include <vector>  
+#include <random> 
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+  
+int main() {  
+    cv::Mat srcMat = imread("count.jpg");
+    cv::Mat resMat;
+	cv::Mat resover;
+	cv::Mat resover_dec;
+	cv::Mat resover_sign;
+	cv::Mat Matstate;
+    cv::Mat center;
+    cv::Mat gray;  
+
+    cv::cvtColor(srcMat, gray, cv::COLOR_BGR2GRAY);  
+	cv::threshold(gray, resMat, 100, 255, THRESH_OTSU);
+	cv::bitwise_not(resMat, resover);
+	cv::connectedComponentsWithStats(resover, resover_sign, Matstate, center, 8, CV_32S);
+
+	int totalClips = 0;
+	int i;
+    int line=0;
+	line = Matstate.rows;
+
+    for(i=0;i< Matstate.rows;i++){
+        if (Matstate.at<int>(i, 4) >= 2000&& Matstate.at<int>(i, 4)<=8000){
+            cv::Rect rect;
+
+            rect.x = Matstate.at<int>(i, 0);
+			rect.y = Matstate.at<int>(i, 1);
+			rect.width = Matstate.at<int>(i, 2);
+			rect.height = Matstate.at<int>(i, 3);
+
+            cv::rectangle(srcMat, rect,cv::Scalar(0, 0, 200),2,8,0); 
+
+            std::stringstream ss;  
+            ss << " " << (++totalClips);  
+            cv::putText(srcMat, ss.str(), cv::Point(rect.x + 5, rect.y + 20),  
+            cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 200), 2);  
+        }
+    }
+    
+    cv::putText(srcMat, "Total clips: " + std::to_string(totalClips), cv::Point(10, 30),  
+                cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 200), 2);  
+  
+    cv::imshow("Clips", srcMat);  
+  
+    cv::waitKey(0);  
+    
+    return 0;  
+}
